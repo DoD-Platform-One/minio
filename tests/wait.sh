965fc77eea05a33dc10c5c1ef80d8a066e08335a
Branden Cobb <cobb_branden@bah.com>
2021-10-27 18:50:20 +0000

    Modify namespace in wait script

--- tests/wait.sh
+++ tests/wait.sh
@@ -2,13 +2,16 @@
 wait_project() {
    timeElapsed=0
    while true; do
-      resourceHealth=$(kubectl get tenant -A -o jsonpath='{.items[0].status.healthStatus}' | xargs)     # Update with the resource to check and jsonpath
+      resourceHealth=$(kubectl get tenant -n minio -o jsonpath='{.items[0].status.healthStatus}' | xargs)     # Update with the resource to check and jsonpath
       if [[ $resourceHealth == "green" ]]; then                                             # Update with desired health/output of the jsonpath
+        echo "minio custom resource creation finished"
          break
       fi
       sleep 5
       timeElapsed=$(($timeElapsed+5))
       if [[ $timeElapsed -ge 600 ]]; then
+         echo "timeout waiting 600 seconds for minio resource creation, running describe..." 1>&2
+         kubectl describe tenant -n minio 1>&2
          exit 1
       fi
    done
